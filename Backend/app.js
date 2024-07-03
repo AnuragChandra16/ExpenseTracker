ed36c8714bea32d686520a016b84ea97db9560a7
Anurag <anuragchandra1601@gmail.com>
2024-07-03 12:52:37 +0530

    first commit

--- Backend/app.js
+++ Backend/app.js
@@ -1,31 +1,36 @@
-const express=require('express');
-const mongoose =require("mongoose");
-const cors=require("cors");
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
 const userRouter = require('./routes/userRouter');
 const errorHandler = require('./middlewares/errorHandlerMiddleware');
-//const categoryController = require('./controllers/categoryCtrl');
 const categoryRouter = require('./routes/categoryRouter');
 const transactionRouter = require('./routes/transactionRouter');
 
-const app=express();
+const app = express();
 
-//connect to mongodb
-mongoose.connect("mongodb+srv://anuragchandra1601:7w1V1yFlYG1bSEq6@expensetracker.80gvpvz.mongodb.net/?retryWrites=true&w=majority&appName=Expensetracker")
-.then(()=>console.log("connected to mongodb"))
-.catch(err=>console.log(err));
-//cors config
-const corsOptions={
-    origin:["http://localhost:5173"],
+// connect to mongodb
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("connected to mongodb"))
+  .catch(err => console.log(err));
+
+// cors config
+const corsOptions = {
+    origin: ["http://localhost:5173"],
 };
 app.use(cors(corsOptions));
-//middlewares
-app.use(express.json());//pass incoming json data
-//routes
-app.use("/",userRouter);
-app.use("/",categoryRouter);
-app.use("/",transactionRouter);
-//error
+
+// middlewares
+app.use(express.json());
+
+// routes
+app.use("/", userRouter);
+app.use("/", categoryRouter);
+app.use("/", transactionRouter);
+
+// error
 app.use(errorHandler);
-//start server
-const PORT=process.env.PORT || 8000;
-app.listen(PORT,()=>console.log(`Server is running on this port ${PORT}`));
+
+// start server
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server is running on this port ${PORT}`));
