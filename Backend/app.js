99ac6a286b6b3e9f37ef8daf72c7d38dc0268ff7
Anurag <anuragchandra1601@gmail.com>
2024-07-04 13:23:40 +0530

    eighth commit

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGODB_URI)
 
 // CORS config
 const corsOptions = {
-    origin: ["http://localhost:5173", "*"],
+    origin: ["https://expense-tracker-azure-eta.vercel.app/"],//["http://localhost:5173", "*"],
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify the methods you want to allow
     credentials: true // Allow cookies to be sent with requests
 };
